23e2ad5972922d40ea3e9c02ba37060ba5b33320
estudiante <estudiante@ISIS2603-320.sis.virtual.uniandes.edu.co>
2018-08-28 17:53:22 -0500

    Resources Tematica y Servicio (GET, POST, PUT, DELETE vacios)

--- s4_PartyServices-api/src/main/java/co/edu/uniandes/csw/partyServices/resources/TematicaResource.java
+++ s4_PartyServices-api/src/main/java/co/edu/uniandes/csw/partyServices/resources/TematicaResource.java
@@ -6,18 +6,88 @@
 package co.edu.uniandes.csw.partyServices.resources;
 
 import co.edu.uniandes.csw.partyServices.dtos.TematicaDTO;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 
 /**
  *
- * @author estudiante
+ * @author Tomas Vargas
  */
+@Path("tematicas")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
 public class TematicaResource {
     
-    @GET
-    public TematicaDTO consultarTematica()
-{
-    return new TematicaDTO();
-}
+    private static final Logger LOGGER = Logger.getLogger(TematicaResource.class.getName());
     
+    //@Inject
+    /* TematicaLogic tematicaLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias. */
+    
+    /**
+     * Busca la tematica con el id asociado recibido en la URL y la devuelve.
+     *
+     * @param tematicasId Identificador de la tematica que se esta buscando.
+     * Este debe ser una cadena de dígitos.
+     * @return JSON {@link TematicaDTO} - La tematica buscada
+     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
+     * Error de lógica que se genera cuando no se encuentra la tematica.
+     */
+    @GET
+    @Path("{tematicasId: \\d+}")
+    public TematicaDTO getTematica(@PathParam("tematicasId") Long tematicasId) throws WebApplicationException {
+        return new TematicaDTO();
+    }
+ 
+    /**
+     * Crea una nueva tematica con la informacion que se recibe en el cuerpo de
+     * la petición y se regresa un objeto identico con un id auto-generado por
+     * la base de datos.
+     *
+     * @param tematica {@link TematicaDTO} - La tematica que se desea
+     * guardar.
+     * @return JSON {@link TematicaDTO} - La tematica guardada con el atributo
+     * id autogenerado.
+     */
+    @POST
+    public TematicaDTO crearTematica(TematicaDTO tematica) throws WebApplicationException{
+        return tematica;
+    }
+    
+    /**
+     * Modifica tematica con la informacion que se recibe en el cuerpo de
+     * la petición y se regresa un objeto identico.
+     *
+     * @param tematicasId Identificador de la tematica que se desea modificar.
+     * @param tematica {@link TematicaDTO} - La tematica que se desea
+     * modificar.
+     * @return JSON {@link TematicaDTO} - La tematica modificada.
+     */
+    @PUT
+    @Path("{tematicasId: \\d+}")
+    public TematicaDTO modificarTematica(@PathParam("tematicasId") Long tematicasId, TematicaDTO nuevaTematica) throws WebApplicationException{
+        return nuevaTematica;
+    }
+    
+     /**
+     * Borra la tematica con el id asociado recibido en la URL.
+     *
+     * @param tematicasId Identificador de la tematica que se desea borrar.
+     * Este debe ser una cadena de dígitos.
+     */
+    @DELETE
+    @Path("{tematicasId: \\d+}")
+    public void deleteTematica(@PathParam("tematicasId") Long tematicasId) throws WebApplicationException {
+        
+    }
 }
