1ab7cb6404811a80e8a207dff2bf94bd96663ce9
estudiante <estudiante@ISIS2603-337.sis.virtual.uniandes.edu.co>
2018-09-02 18:37:08 -0500

    Inicio de pruebas del persistence

--- s4_PartyServices-back/src/test/java/AgendaPersistenceTest.java
+++ s4_PartyServices-back/src/test/java/AgendaPersistenceTest.java
@@ -1,3 +1,21 @@
+
+import co.edu.uniandes.csw.partyServices.entities.AgendaEntity;
+import co.edu.uniandes.csw.partyServices.persistence.AgendaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.glassfish.internal.deployment.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +26,87 @@
  *
  * @author estudiante
  */
+@RunWith(Arquillian.class)
 public class AgendaPersistenceTest {
     
+    @Inject
+    private AgendaPersistence agendaPersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    /**
+     * Variable para martcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+    
+    
+    private List<AgendaEntity> data = new ArrayList<AgendaEntity>();
+    
+     /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Editorial, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @org.jboss.arquillian.container.test.api.Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(AgendaEntity.class.getPackage())
+                .addPackage(AgendaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     *
+     */
+    private void clearData() {
+        em.createQuery("delete from EditorialEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     *
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+
+            AgendaEntity entity = factory.manufacturePojo(AgendaEntity.class);
+
+            em.persist(entity);
+
+            data.add(entity);
+        }
+    }
+
 }
--- s4_PartyServices-back/src/test/java/FechaPersistenceTest.java
+++ s4_PartyServices-back/src/test/java/FechaPersistenceTest.java
@@ -1,3 +1,11 @@
+
+import co.edu.uniandes.csw.partyServices.persistence.FechaPersistence;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.runner.RunWith;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +16,11 @@
  *
  * @author estudiante
  */
+@RunWith(Arquillian.class)
 public class FechaPersistenceTest {
+    @Inject
+    private FechaPersistence fechaPersistence;
     
+    @PersistenceContext
+    private EntityManager em;
 }
