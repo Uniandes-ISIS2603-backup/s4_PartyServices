d89680cf6fb7fded55f48846fec9aaceec907cfe
estudiante <estudiante@ISIS2603-320.sis.virtual.uniandes.edu.co>
2018-09-15 16:56:18 -0500

    TematicaLogicTest y ServicioLogicTest (Falta método de proveedores)

---
+++ s4_PartyServices-back/src/test/java/logic/ServicioLogicTest.java
@@ -0,0 +1,206 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import co.edu.uniandes.csw.partyServices.ejb.ServicioLogic;
+import co.edu.uniandes.csw.partyServices.entities.ProveedorEntity;
+import co.edu.uniandes.csw.partyServices.entities.ServicioEntity;
+import co.edu.uniandes.csw.partyServices.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.partyServices.persistence.ServicioPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.junit.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ * Pruebas de lógica de Servicio
+ * @author Tomas Vargas
+ */
+
+@RunWith(Arquillian.class)
+public class ServicioLogicTest {
+    
+    private PodamFactory factory = new PodamFactoryImpl();
+
+    @Inject
+    private ServicioLogic servicioLogic;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    private UserTransaction utx;
+
+    private List<ServicioEntity> data = new ArrayList<>();
+
+    /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ServicioEntity.class.getPackage())
+                .addPackage(ServicioLogic.class.getPackage())
+                .addPackage(ServicioPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from PrizeEntity").executeUpdate();
+        em.createQuery("delete from ProveedorEntity").executeUpdate();
+        em.createQuery("delete from ServicioEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            ServicioEntity entity = factory.manufacturePojo(ServicioEntity.class);
+            em.persist(entity);
+            entity.setProveedores(new ArrayList<>());
+            data.add(entity);
+        }
+        ServicioEntity servicio = data.get(2);
+        ProveedorEntity entity = factory.manufacturePojo(ProveedorEntity.class);
+        
+        // -------------------------------------------------------------- ERROR ------------------------------------------------------------//
+        
+        //entity.getServicios().add(servicio);
+        
+        // -------------------------------------------------------------- ERROR ------------------------------------------------------------//
+        
+        em.persist(entity);
+        servicio.getProveedores().add(entity);
+    }
+
+    /**
+     * Prueba para crear un Servicio.
+     */
+    @Test
+    public void createServicioTest() throws BusinessLogicException {
+        ServicioEntity newEntity = factory.manufacturePojo(ServicioEntity.class);
+        ServicioEntity result = servicioLogic.createServicio(newEntity);
+        Assert.assertNotNull(result);
+        ServicioEntity entity = em.find(ServicioEntity.class, result.getId());
+        Assert.assertEquals(newEntity.getId(), entity.getId());
+        Assert.assertEquals(newEntity.getTipo(), entity.getTipo());
+        
+    }
+
+    /**
+     * Prueba para consultar la lista de Servicios.
+     */
+    @Test
+    public void getServiciosTest() {
+        List<ServicioEntity> list = servicioLogic.getServicios();
+        Assert.assertEquals(data.size(), list.size());
+        for (ServicioEntity entity : list) {
+            boolean found = false;
+            for (ServicioEntity storedEntity : data) {
+                if (entity.getId().equals(storedEntity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+
+    /**
+     * Prueba para consultar un Servicio.
+     */
+    @Test
+    public void getServicioTest() {
+        ServicioEntity entity = data.get(0);
+        ServicioEntity resultEntity = servicioLogic.getServicio(entity.getId());
+        Assert.assertNotNull(resultEntity);
+        Assert.assertEquals(entity.getId(), resultEntity.getId());
+        Assert.assertEquals(entity.getTipo(), resultEntity.getTipo());
+        
+    }
+
+    /**
+     * Prueba para actualizar un Servicio.
+     */
+    @Test
+    public void updateServicioTest() {
+        ServicioEntity entity = data.get(0);
+        ServicioEntity pojoEntity = factory.manufacturePojo(ServicioEntity.class);
+
+        pojoEntity.setId(entity.getId());
+
+        servicioLogic.updateServicio(pojoEntity.getId(), pojoEntity);
+
+        ServicioEntity resp = em.find(ServicioEntity.class, entity.getId());
+
+        Assert.assertEquals(pojoEntity.getId(), resp.getId());
+        Assert.assertEquals(pojoEntity.getTipo(), resp.getTipo());
+        
+    }
+
+    /**
+     * Prueba para eliminar un Servicio
+     *
+     * @throws co.edu.uniandes.csw.proveedorstore.exceptions.BusinessLogicException
+     */
+    @Test
+    public void deleteServicioTest() throws BusinessLogicException {
+        ServicioEntity entity = data.get(0);
+        servicioLogic.deleteServicio(entity.getId());
+        ServicioEntity deleted = em.find(ServicioEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+
+    /**
+     * Prueba para eliminar un Servicio asociado a un proveedor
+     *
+     * @throws co.edu.uniandes.csw.proveedorstore.exceptions.BusinessLogicException
+     */
+    @Test(expected = BusinessLogicException.class)
+    public void deleteServicioConProveedorTest() throws BusinessLogicException {
+        servicioLogic.deleteServicio(data.get(2).getId());
+    }
+    
+}
