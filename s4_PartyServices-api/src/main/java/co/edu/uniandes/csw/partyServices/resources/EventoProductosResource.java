bc17721d80c0c86859ec701584a558b22c6df8c6
estudiante <estudiante@ISIS2603-320.sis.virtual.uniandes.edu.co>
2018-11-27 12:56:44 -0500

    EventoProductos v.2

--- s4_PartyServices-api/src/main/java/co/edu/uniandes/csw/partyServices/resources/EventoProductosResource.java
+++ s4_PartyServices-api/src/main/java/co/edu/uniandes/csw/partyServices/resources/EventoProductosResource.java
@@ -5,10 +5,13 @@
  */
 package co.edu.uniandes.csw.partyServices.resources;
 
-import co.edu.uniandes.csw.partyServices.dtos.ProveedorDTO;
-import co.edu.uniandes.csw.partyServices.dtos.ProveedorDetailDTO;
+import co.edu.uniandes.csw.partyServices.dtos.ProductoDTO;
+import co.edu.uniandes.csw.partyServices.dtos.ProductoDetailDTO;
+import co.edu.uniandes.csw.partyServices.ejb.EventoProductosLogic;
+import co.edu.uniandes.csw.partyServices.ejb.ProductoLogic;
 import co.edu.uniandes.csw.partyServices.ejb.ProveedorLogic;
 import co.edu.uniandes.csw.partyServices.ejb.ServicioProveedoresLogic;
+import co.edu.uniandes.csw.partyServices.entities.ProductoEntity;
 import co.edu.uniandes.csw.partyServices.entities.ProveedorEntity;
 import co.edu.uniandes.csw.partyServices.exceptions.BusinessLogicException;
 import java.util.ArrayList;
@@ -17,6 +20,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -35,128 +39,140 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class EventoProductosResource {
     
-    private static final Logger LOGGER = Logger.getLogger(ServicioProveedoresResource.class.getName());
+   private static final Logger LOGGER = Logger.getLogger(EventoProductosResource.class.getName());
 
     @Inject
-    private ServicioProveedoresLogic servicioProveedoresLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
+    private EventoProductosLogic eventoProductoLogic;
 
     @Inject
-    private ProveedorLogic proveedorLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
+    private ProductoLogic productoLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
 
     /**
-     * Guarda un libro dentro de una servicio con la informacion que recibe el
-     * la URL. Se devuelve el libro que se guarda en la servicio.
+     * Asocia un producto existente con un autor existente
      *
-     * @param serviciosId Identificador de la servicio que se esta
-     * actualizando. Este debe ser una cadena de dígitos.
-     * @param proveedoresId Identificador del libro que se desea guardar. Este debe
-     * ser una cadena de dígitos.
-     * @return JSON {@link ProveedorDTO} - El libro guardado en la servicio.
+     * @param eventosId El ID del autor al cual se le va a asociar el producto
+     * @param productosId El ID del producto que se asocia
+     * @return JSON {@link ProductoDetailDTO} - El producto asociado.
      * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
-     * Error de lógica que se genera cuando no se encuentra el libro.
+     * Error de lógica que se genera cuando no se encuentra el producto.
      */
     @POST
-    @Path("{proveedoresId: \\d+}")
-    public ProveedorDTO addProveedor(@PathParam("serviciosId") Long serviciosId, @PathParam("proveedoresId") Long proveedoresId) {
-        LOGGER.log(Level.INFO, "ServicioProveedoresResource addProveedor: input: serviciosID: {0} , proveedoresId: {1}", new Object[]{serviciosId, proveedoresId});
-        if (proveedorLogic.getProveedor(proveedoresId) == null) {
-            throw new WebApplicationException("El recurso /proveedores/" + proveedoresId + " no existe.", 404);
+    @Path("{productosId: \\d+}")
+    public ProductoDetailDTO addProducto(@PathParam("eventosId") Long eventosId, @PathParam("productosId") Long productosId) {
+        LOGGER.log(Level.INFO, "EventoProductosResource addProducto: input: eventosId {0} , productosId {1}", new Object[]{eventosId, productosId});
+        if (productoLogic.getProductoId(productosId) == null) {
+            throw new WebApplicationException("El recurso /productos/" + productosId + " no existe.", 404);
         }
-        ProveedorDTO proveedorDTO = new ProveedorDTO(servicioProveedoresLogic.addProveedor(proveedoresId, serviciosId));
-        LOGGER.log(Level.INFO, "ServicioProveedoresResource addProveedor: output: {0}", proveedorDTO);
-        return proveedorDTO;
+        ProductoDetailDTO detailDTO = new ProductoDetailDTO(eventoProductoLogic.addProducto(eventosId, productosId));
+        LOGGER.log(Level.INFO, "EventoProductosResource addProducto: output: {0}", detailDTO);
+        return detailDTO;
     }
 
     /**
-     * Busca y devuelve todos los libros que existen en la servicio.
+     * Busca y devuelve todos los productos que existen en un autor.
      *
-     * @param serviciosId Identificador de la servicio que se esta buscando.
-     * Este debe ser una cadena de dígitos.
-     * @return JSONArray {@link ProveedorDetailDTO} - Los libros encontrados en la
-     * servicio. Si no hay ninguno retorna una lista vacía.
+     * @param eventosId El ID del autor del cual se buscan los productos
+     * @return JSONArray {@link ProductoDetailDTO} - Los productos encontrados en el
+     * autor. Si no hay ninguno retorna una lista vacía.
      */
     @GET
-    public List<ProveedorDetailDTO> getProveedores(@PathParam("serviciosId") Long serviciosId) {
-        LOGGER.log(Level.INFO, "ServicioProveedoresResource getProveedores: input: {0}", serviciosId);
-        List<ProveedorDetailDTO> listaDetailDTOs = proveedoresListEntity2DTO(servicioProveedoresLogic.getProveedores(serviciosId));
-        LOGGER.log(Level.INFO, "ServicioProveedoresResource getProveedores: output: {0}", listaDetailDTOs);
-        return listaDetailDTOs;
+    public List<ProductoDetailDTO> getProductos(@PathParam("eventosId") Long eventosId) {
+        LOGGER.log(Level.INFO, "EventoProductosResource getProductos: input: {0}", eventosId);
+        List<ProductoDetailDTO> lista = productosListEntity2DTO(eventoProductoLogic.getProductos(eventosId));
+        LOGGER.log(Level.INFO, "EventoProductosResource getProductos: output: {0}", lista);
+        return lista;
     }
 
     /**
-     * Busca el libro con el id asociado dentro de la servicio con id asociado.
+     * Busca y devuelve el producto con el ID recibido en la URL, relativo a un
+     * autor.
      *
-     * @param serviciosId Identificador de la servicio que se esta buscando.
-     * Este debe ser una cadena de dígitos.
-     * @param proveedoresId Identificador del libro que se esta buscando. Este debe
-     * ser una cadena de dígitos.
-     * @return JSON {@link ProveedorDetailDTO} - El libro buscado
+     * @param eventosId El ID del autor del cual se busca el producto
+     * @param productosId El ID del producto que se busca
+     * @return {@link ProductoDetailDTO} - El producto encontrado en el autor.
+     * @throws co.edu.uniandes.csw.productostore.exceptions.BusinessLogicException
+     * si el producto no está asociado al autor
      * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
-     * Error de lógica que se genera cuando no se encuentra el libro.
-     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} -
-     * Error de lógica que se genera cuando no se encuentra el libro en la
-     * servicio.
+     * Error de lógica que se genera cuando no se encuentra el producto.
      */
     @GET
-    @Path("{proveedoresId: \\d+}")
-    public ProveedorDetailDTO getProveedor(@PathParam("serviciosId") Long serviciosId, @PathParam("proveedoresId") Long proveedoresId) throws BusinessLogicException {
-        LOGGER.log(Level.INFO, "ServicioProveedoresResource getProveedor: input: serviciosID: {0} , proveedoresId: {1}", new Object[]{serviciosId, proveedoresId});
-        if (proveedorLogic.getProveedor(proveedoresId) == null) {
-            throw new WebApplicationException("El recurso /servicios/" + serviciosId + "/proveedores/" + proveedoresId + " no existe.", 404);
+    @Path("{productosId: \\d+}")
+    public ProductoDetailDTO getProducto(@PathParam("eventosId") Long eventosId, @PathParam("productosId") Long productosId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "EventoProductosResource getProducto: input: eventosId {0} , productosId {1}", new Object[]{eventosId, productosId});
+        if (productoLogic.getProductoId(productosId) == null) {
+            throw new WebApplicationException("El recurso /productos/" + productosId + " no existe.", 404);
         }
-        ProveedorDetailDTO proveedorDetailDTO = new ProveedorDetailDTO(servicioProveedoresLogic.getProveedor(serviciosId, proveedoresId));
-        LOGGER.log(Level.INFO, "ServicioProveedoresResource getProveedor: output: {0}", proveedorDetailDTO);
-        return proveedorDetailDTO;
+        ProductoDetailDTO detailDTO = new ProductoDetailDTO(eventoProductoLogic.getProducto(eventosId, productosId));
+        LOGGER.log(Level.INFO, "EventoProductosResource getProducto: output: {0}", detailDTO);
+        return detailDTO;
     }
 
     /**
-     * Remplaza las instancias de Proveedor asociadas a una instancia de Servicio
+     * Actualiza la lista de productos de un autor con la lista que se recibe en el
+     * cuerpo
      *
-     * @param serviciosId Identificador de la servicio que se esta
-     * remplazando. Este debe ser una cadena de dígitos.
-     * @param proveedores JSONArray {@link ProveedorDTO} El arreglo de libros nuevo para la
-     * servicio.
-     * @return JSON {@link ProveedorDTO} - El arreglo de libros guardado en la
-     * servicio.
+     * @param eventosId El ID del autor al cual se le va a asociar el producto
+     * @param productos JSONArray {@link ProductoDetailDTO} - La lista de productos que se
+     * desea guardar.
+     * @return JSONArray {@link ProductoDetailDTO} - La lista actualizada.
      * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
-     * Error de lógica que se genera cuando no se encuentra el libro.
+     * Error de lógica que se genera cuando no se encuentra el producto.
      */
     @PUT
-    public List<ProveedorDetailDTO> replaceProveedores(@PathParam("serviciosId") Long serviciosId, List<ProveedorDetailDTO> proveedores) {
-        LOGGER.log(Level.INFO, "ServicioProveedoresResource replaceProveedores: input: serviciosId: {0} , proveedores: {1}", new Object[]{serviciosId, proveedores});
-        for (ProveedorDetailDTO proveedor : proveedores) {
-            if (proveedorLogic.getProveedor(proveedor.getId()) == null) {
-                throw new WebApplicationException("El recurso /proveedores/" + proveedor.getId() + " no existe.", 404);
+    public List<ProductoDetailDTO> replaceProductos(@PathParam("eventosId") Long eventosId, List<ProductoDetailDTO> productos) {
+        LOGGER.log(Level.INFO, "EventoProductosResource replaceProductos: input: eventosId {0} , productos {1}", new Object[]{eventosId, productos});
+        for (ProductoDetailDTO producto : productos) {
+            if (productoLogic.getProductoId(producto.getId()) == null) {
+                throw new WebApplicationException("El recurso /productos/" + producto.getId() + " no existe.", 404);
             }
         }
-        List<ProveedorDetailDTO> listaDetailDTOs = proveedoresListEntity2DTO(servicioProveedoresLogic.replaceProveedores(serviciosId, proveedoresListDTO2Entity(proveedores)));
-        LOGGER.log(Level.INFO, "ServicioProveedoresResource replaceProveedores: output: {0}", listaDetailDTOs);
-        return listaDetailDTOs;
+        List<ProductoDetailDTO> lista = productosListEntity2DTO(eventoProductoLogic.replaceProductos(eventosId, productosListDTO2Entity(productos)));
+        LOGGER.log(Level.INFO, "EventoProductosResource replaceProductos: output: {0}", lista);
+        return lista;
     }
 
     /**
-     * Convierte una lista de ProveedorEntity a una lista de ProveedorDetailDTO.
+     * Elimina la conexión entre el producto y e autor recibidos en la URL.
      *
-     * @param entityList Lista de ProveedorEntity a convertir.
-     * @return Lista de ProveedorDTO convertida.
+     * @param eventosId El ID del autor al cual se le va a desasociar el producto
+     * @param productosId El ID del producto que se desasocia
+     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
+     * Error de lógica que se genera cuando no se encuentra el producto.
      */
-    private List<ProveedorDetailDTO> proveedoresListEntity2DTO(List<ProveedorEntity> entityList) {
-        List<ProveedorDetailDTO> list = new ArrayList();
-        for (ProveedorEntity entity : entityList) {
-            list.add(new ProveedorDetailDTO(entity));
+    @DELETE
+    @Path("{productosId: \\d+}")
+    public void removeProducto(@PathParam("eventosId") Long eventosId, @PathParam("productosId") Long productosId) {
+        LOGGER.log(Level.INFO, "EventoProductosResource deleteProducto: input: eventosId {0} , productosId {1}", new Object[]{eventosId, productosId});
+        if (productoLogic.getProductoId(productosId) == null) {
+            throw new WebApplicationException("El recurso /productos/" + productosId + " no existe.", 404);
+        }
+        eventoProductoLogic.removeProducto(eventosId, productosId);
+        LOGGER.info("EventoProductosResource deleteProducto: output: void");
+    }
+
+    /**
+     * Convierte una lista de ProductoEntity a una lista de ProductoDetailDTO.
+     *
+     * @param entityList Lista de ProductoEntity a convertir.
+     * @return Lista de ProductoDetailDTO convertida.
+     */
+    private List<ProductoDetailDTO> productosListEntity2DTO(List<ProductoEntity> entityList) {
+        List<ProductoDetailDTO> list = new ArrayList<>();
+        for (ProductoEntity entity : entityList) {
+            list.add(new ProductoDetailDTO(entity));
         }
         return list;
     }
 
     /**
-     * Convierte una lista de ProveedorDetailDTO a una lista de ProveedorEntity.
+     * Convierte una lista de ProductoDetailDTO a una lista de ProductoEntity.
      *
-     * @param dtos Lista de ProveedorDetailDTO a convertir.
-     * @return Lista de ProveedorEntity convertida.
+     * @param dtos Lista de ProductoDetailDTO a convertir.
+     * @return Lista de ProductoEntity convertida.
      */
-    private List<ProveedorEntity> proveedoresListDTO2Entity(List<ProveedorDetailDTO> dtos) {
-        List<ProveedorEntity> list = new ArrayList<>();
-        for (ProveedorDetailDTO dto : dtos) {
+    private List<ProductoEntity> productosListDTO2Entity(List<ProductoDetailDTO> dtos) {
+        List<ProductoEntity> list = new ArrayList<>();
+        for (ProductoDetailDTO dto : dtos) {
             list.add(dto.toEntity());
         }
         return list;
